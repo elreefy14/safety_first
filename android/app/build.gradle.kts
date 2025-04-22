6680d612dfe922fbf5053fecd7804a511589dd2d
elreefy14 <es-Ahmedhossameldin2024@alexu.edu.eg>
2025-04-22 16:07:29 +0200

    com.safety.first2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.safety_frist"
+    namespace = "com.safety.first2"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13113456"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.safety_frist"
+        applicationId = "com.safety.first2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(21)
--- android/app/src/main/kotlin/com/example/safety_frist/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.safety_frist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
